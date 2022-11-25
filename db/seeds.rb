9dad501c0fdbb1bbd1f1791e30fc602d627f8dab
Michelle Madrid <msmadrid@uc.cl>
2022-11-24 22:02:54 -0300

    almost ready

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.create([{ name: 'Restaurante Chino', address: 'Avenida pajaritos 123',
+                     phone_number: '1265985', category: 'chinese' },
+                   { name: 'Restaurante Italiano', address: 'Avenida pajaritos 124',
+                     phone_number: '1265986', category: 'italian' },
+                   { name: 'Restaurante Japonés', address: 'Avenida pajaritos 125',
+                     phone_number: '12659857', category: 'japanese' },
+                   { name: 'Restaurante Francés', address: 'Avenida pajaritos 126',
+                     phone_number: '1265988', category: 'french' },
+                   { name: 'Restaurante Belga', address: 'Avenida pajaritos 127',
+                     phone_number: '1265989', category: 'belgian' }])
